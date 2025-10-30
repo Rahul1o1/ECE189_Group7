043d1450752dafb735005204c0fa023631bc6136
Renish-UCLA <renish.israel@gmail.com>
2025-10-30 13:53:06 -0700

    Added comments in priority_encoder.sv

--- phase0/priority_encoder.sv
+++ phase0/priority_encoder.sv
@@ -10,18 +10,18 @@ module priority_encoder
     output wire valid
     );
 
-assign valid = |in;
+assign valid = |in; // input signal is invalid if all the input bits are 0
 integer i;
 reg flag = 0;
 reg[$clog2(WIDTH) - 1: 0] out_reg = WIDTH - 1;
 
-always @(*) begin
+	always @(*) begin // combinational circuit definition
 	flag = 0;
 	out_reg = 0;
-	for(i = WIDTH - 1; i >= 0; i = i - 1) begin
+	for(i = WIDTH - 1; i >= 0; i = i - 1) begin // loop begins from the MSB of the input to find which is the most significant bit that is set to 1
 		if((in[i] == 1) && (!flag)) begin
 			flag = 1;
-			out_reg = i;
+			out_reg = i; // output is the binary equivalent of the value i (which is in decimal), that marks the first location from the MSB that has a bit 1
 		end
 		else begin
 			flag = flag;
